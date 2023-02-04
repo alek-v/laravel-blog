36c12a3226aef1fd474e961ad0b9a26034bd27d2
Aleksandar Vranesevic <bg.aleksandar@gmail.com>
2023-02-04 13:56:16 +0100

    refactor: Use blade components for layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,14 @@
-<!doctype html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/app.css">
-    <title>Laravel Blog</title>
-</head>
-<body>
-    <h1>Hello Laravel</h1>
-    @foreach ($posts as $post)
-        <article>
-            <h2><a href="/posts/{{ $post->link }}">{{ $post->title }}</a></h2>
-            <p>{{ $post->description }}</p>
-        </article>
-    @endforeach
-</body>
-</html>
+{{-- Using yield for layout --}}
+@extends('layout')
+
+@section('content')
+
+<h1>Hello Laravel</h1>
+@foreach ($posts as $post)
+    <article>
+        <h2><a href="/posts/{{ $post->link }}">{{ $post->title }}</a></h2>
+        <p>{{ $post->description }}</p>
+    </article>
+@endforeach
+
+@endsection
