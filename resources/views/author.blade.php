c3ee78f8f685db909bee91e8f4ecc961278cbd78
Aleksandar Vranesevic <bg.aleksandar@gmail.com>
2023-02-15 10:22:32 +0100

    style: Remove unused CSS file

--- resources/views/author.blade.php
+++ resources/views/author.blade.php
@@ -1,13 +1,15 @@
 <x-layout>
-    @foreach ($posts as $post)
-        <article>
-            <p><a href="/posts/{{ $post->slug }}">{{ $post->title }}</a></p>
-            <p>By <a href="{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
-            <p>{{ $post->description }}</p>
-        </article>
-    @endforeach
-
-    <div>
-        <p><a href="/">Home page</a></p>
-    </div>
+    @include('_posts-header')
+    <main class="container">
+        @foreach ($posts as $post)
+            <article>
+                <p><a href="/posts/{{ $post->slug }}">{{ $post->title }}</a></p>
+                <p>By <a href="{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+                <p>{{ $post->description }}</p>
+            </article>
+        @endforeach
+        <div>
+            <p><a href="/">Home page</a></p>
+        </div>
+    </main>
 </x-layout>
