bd658a5c3d2a785e36bb187a4c2f214c6e0411a8
Aleksandar Vranesevic <bg.aleksandar@gmail.com>
2023-02-23 17:37:51 +0100

    refactor: Show form error message

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -4,7 +4,10 @@
         <div class="d-flex justify-content-center mb-5 mt-5">
             <form method="POST" action="/register">
                 <label for="name" class="form-label">Name:</label>
-                <input id="name" name="name" type="text" value="" class="form-control mb-3" />
+                <input id="name" name="name" type="text" value="" class="form-control mb-3" aria-describedby="name-error" />
+                @error('name')
+                <div class="mb-3"><small id="name-error" class="form-text text-danger">{{ $message }}</small></div>
+                @enderror
                 <label for="username" class="form-label">Username:</label>
                 <input id="username" name="username" type="text" class="form-control mb-3" />
                 <label for="email" class="form-label">Email:</label>
